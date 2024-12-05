6053f174bbf7f5486dcc89fd5b543180d021a3c9
Giuseppe Mancini <giuseppe.mancini80@gmail.com>
2024-12-05 15:53:02 +0100

    I created the array of Pokemon objects | name - height - types

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
-alert('Hello world');
+// I created the array of Pokemon objects | name - height - types
 
-let favoriteFood = "Pizza";
-
-document.write(favoriteFood);
+let pokemonList = [
+    { name: 'Charizard', height: 6, types: ['steel', 'bug'] },
+    { name: 'Arbok', height: 4, types: ['fairy', 'grass'] },
+    { name: 'Kadabra', height: 5, types: ['fighting', 'psychic'] }
+];
