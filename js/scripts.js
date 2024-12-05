122f4dc43ef01615cba769c9ba04aeaa4d73f744
Giuseppe Mancini <giuseppe.mancini80@gmail.com>
2024-12-05 15:23:33 +0100

    pokemonList added in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
-alert('Hello world');
-
-let favoriteFood = "Pizza";
-
-document.write(favoriteFood);
+let pokemonList = [
+    { name: Charizard, height: 6, type: [steel, bug] },
+    { name: Arbok, height: 4, type: [fairy, grass] },
+    { name: Kadabra, height: 5, type: [fighting, psychic] }
+];
