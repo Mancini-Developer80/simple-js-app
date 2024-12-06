928e2e0bfd306b3c6317a40e4b73bfd602c85d07
Giuseppe Mancini <giuseppe.mancini80@gmail.com>
2024-12-06 12:15:32 +0100

    outputing Pokemon list and CSS style added

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,22 @@
 // I created the array of Pokemon objects | name - height - types
 
 let pokemonList = [
-    { name: 'Charizard', height: 6, types: ['steel', 'bug'] },
-    { name: 'Arbok', height: 4, types: ['fairy', 'grass'] },
-    { name: 'Kadabra', height: 5, types: ['fighting', 'psychic'] }
+    { name: 'Arbok', height: 1.4, types: ['fairy', 'grass'] },
+    { name: 'Kadabra', height: 1.5, types: ['fighting', 'psychic'] },
+    { name: 'Charizard', height: 1.6, types: ['steel', 'bug'] }
 ];
+
+// Iterating over each object of pokemonList array
+// If height > 1.5 outputing alternative text
+
+for (let i = 0; i < pokemonList.length; i++) {
+    let name = pokemonList[i].name;
+    let height = pokemonList[i].height
+
+    let textInside = height > 1.5 ?
+        `<p>Name <span class="card-name ">: ${name}</span> (Height: ${height}) - Wow, that's big! </p>` :
+        `<p>Name <span class="card-name ">: ${name}</span> (Height: ${height})</p>`;
+    
+    document.write(`<div class="card">${textInside}</div>`);
+        
+}
